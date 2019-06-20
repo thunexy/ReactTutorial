35cbfc682fffbbbfd10979c2e153807d6324cdf1
user.name <adetoye.tunex@gmail.com>
2019-06-20 10:19:43 +0100

    Navigation complete with initial code

--- App.js
+++ App.js
@@ -1,73 +1,26 @@
 import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import PlaceList from "./src/components/PlaceList/PlaceList";
-import PlaceInput from "./src/components/PlaceInput/PlaceInput";
-import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
-import {addPlace, selectPlace, deselectPlace, deletePlace} from "./src/store/actions/index";
-import {connect} from "react-redux";
+import {createStackNavigator, createAppContainer} from "react-navigation";
+import {HomeScreen, OtherScreen} from "./src/pages/index";
+export default class App extends Component {
 
-class App extends Component {
+    goToPage = () => {
 
-
-    placeAddedHandler = (placeName) => {
-        this.props.onAddPlace(placeName);
+        this.props.navigation.navigate("Other");
     };
 
-    placeSelectedHandler = (key) => {
-        this.props.onSelectPlace(key);
-
-    };
-
-    placeDeletedHandler = () => {
-        this.props.onDeletePlace();
-    };
-
-
-    modalClosedHandler = () => {
-        this.props.onDeselectPlace();
-    };
 
 
     render() {
-        return (
-            <View style={styles.container}>
-                <PlaceDetail selectedPlace={this.props.selectedPlace} onItemDeleted={this.placeDeletedHandler} onModalClosed={this.modalClosedHandler}/>
-                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-                <PlaceList places={this.props.places} onItemSelected={this.placeSelectedHandler}/>
-
-            </View>
-        );
+        return <AppContainer />;
     }
 
 }
+const RootStack = createStackNavigator({
+    Home: {screen: HomeScreen, navigationOptions: {header: null}},
+    Other: {screen: OtherScreen},
+},
+    {
+        initialRouteName: 'Home',
+    });
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        padding: 26,
-        alignItems: 'center',
-        justifyContent: "flex-start"
-    },
-
-
-
-});
-
-const mapStateToProps = state => {
-    return {
-        places: state.places.places,
-        selectedPlace: state.places.selectedPlace
-    }
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddPlace: (name) => dispatch(addPlace(name)),
-        onDeletePlace: () => dispatch(deletePlace()),
-        onSelectPlace: (key) => dispatch(selectPlace(key)),
-        onDeselectPlace: () => dispatch(deselectPlace())
-    }
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+const AppContainer = createAppContainer(RootStack);
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export HomeScreen from "./HomeScreen";
+export OtherScreen from "./OtherScreen";
